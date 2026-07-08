2da48b3091a965c393e64cf2bf98619a5a4ea336
MarcosEinecke <marcoseinecke@gmail.com>
2026-07-07 21:45:49 -0300

    Conexão com o banco OK

---
+++ Models/Artigo.php
@@ -0,0 +1,15 @@
+<?php
+
+
+class Artigo
+{
+
+
+    public $id;
+    public $titulo;
+    public $dataPublicacao;
+    public $descricao;
+    public $img;
+    public $link;
+    public $id_usuario;
+}
