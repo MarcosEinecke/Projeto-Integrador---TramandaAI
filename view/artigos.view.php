1ab48fbb869bf3f052e2f51c517840b905f8ed56
MarcosEinecke <marcoseinecke@gmail.com>
2026-07-15 02:44:20 -0300

    final

--- view/artigos.view.php
+++ view/artigos.view.php
@@ -6,7 +6,7 @@
         <?php foreach ($pontosTuristicos as $pt): ?>
             <a href="ponto?id=<?= $pt['id'] ?>" class="block">
                 <article class="rounded-2xl overflow-hidden bg-white shadow-md transition-transform transform hover:-translate-y-2 hover:shadow-2xl">
-                    <img class="w-full h-56 object-cover" loading="lazy" src="<?= $pt['url_imagem'] ?>" alt="<?= $pt['nome'] ?>">
+                    <img class="w-full h-56 object-cover" loading="lazy" src="<?= $pt['$pontosTemp => galeria'] ?>" alt="<?= $pt['nome'] ?>">
                     <div class="p-5">
                         <h3 class="font-semibold text-[19px] text-gray-900"><?= $pt['nome'] ?></h3>
                         <p class="mt-2 text-sm leading-relaxed text-gray-600"><?= $pt['descricao'] ?></p>
